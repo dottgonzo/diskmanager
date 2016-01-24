58f8e18acb6f268a33f309fed0ed27215d969692
dario <darioyzf@gmail.com>
2016-01-24 18:48:49 +0100

    without build exec-sync

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 
-let execSync = require("exec-sync");
-
+let execSync = require("sync-exec");
 export = function(){
-    var fdi = execSync("fdisk -l|grep '/dev/s'").split("\n");
+    var fdi = execSync("fdisk -l|grep '/dev/s'").stdout.split("\n");
     let disks = [];
     for (var i = 0; i < fdi.length; i++) {
         if (fdi[i].split("isk").length > 1) {
-            disks.push({ line: fdi[i],sectors_start: fdi[i].replace(/ +(?= )/g,"").split(" ")[1], disk: fdi[i].replace(/ +(?= )/g,"").split(" ")[1].replace(":",""), sectors:fdi[i].replace(/ +(?= )/g,"").split(" ")[6], size:fdi[i].replace(/ +(?= )/g,"").split(" ")[2].replace(",","."), size_unit:fdi[i].replace(/ +(?= )/g,"").split(" ")[3].replace(",",""), partitions: [] });
+            disks.push({ sectors_start: fdi[i].replace(/ +(?= )/g,"").split(" ")[1], disk: fdi[i].replace(/ +(?= )/g,"").split(" ")[1].replace(":",""), sectors:fdi[i].replace(/ +(?= )/g,"").split(" ")[6], size:fdi[i].replace(/ +(?= )/g,"").split(" ")[2].replace(",","."), size_unit:fdi[i].replace(/ +(?= )/g,"").split(" ")[3].replace(",",""), partitions: [] });
         } else {
             let part = { disk: fdi[i].split(" ")[0],sectors_start: fdi[i].replace(/ +(?= )/g,"").split(" ")[1],sectors_stop: fdi[i].replace(/ +(?= )/g,"").split(" ")[2],sectors: fdi[i].replace(/ +(?= )/g,"").split(" ")[3],size: fdi[i].replace(/ +(?= )/g,"").split(" ")[4],typeCode: fdi[i].replace(/ +(?= )/g,"").split(" ")[5] };
             disks[disks.length - 1].partitions.push(part);
